e8cb765871605979e981dbc9e58cee745b62c9e0
James Saucier <jamessaucier@gmail.com>
2020-06-01 23:06:08 -0700

    decimal zero tests

--- test/Forme.Test/DecimalValidationBuilderTest.fs
+++ test/Forme.Test/DecimalValidationBuilderTest.fs
@@ -2,15 +2,28 @@
 
 open NUnit.Framework
 open Forme
+open FsUnit
 
 [<Test>]
 let ``Basic Decimal validation`` () =
-    let validation = validDecimal {
-        atLeast 10M
-    }
-
     12.000001M
-    |> validation
+    |> validDecimal { atLeast 10M }
     |> function
         | Ok -> Assert.Pass()
         | ValidationError e -> failwith "Should have passed"
+
+[<Test>]
+let ``Decimal non zero validation`` () =
+    0.0M
+    |> validDecimal { notZero }
+    |> function
+        | Ok -> failwith "Should have failed validation"
+        | ValidationError e -> e.Message |> should equal "must not equal zero"
+
+[<Test>]
+let ``Decimal zero precision validation`` () =
+    0.00000001M
+    |> validDecimal { notZero }
+    |> function
+        | Ok -> Assert.Pass()
+        | _ -> failwith "Should have failed validation"
