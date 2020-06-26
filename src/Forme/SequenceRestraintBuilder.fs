1d37092a39b6c06cdfca3c3a8560d70e4e26608b
James Saucier <jamessaucier@gmail.com>
2020-06-25 22:08:42 -0700

    add sequence validation builder

---
+++ src/Forme/SequenceRestraintBuilder.fs
@@ -0,0 +1,29 @@
+﻿namespace Forme
+
+open Common
+
+[<AutoOpen>]
+module SequenceRestraintBuilder =
+    
+    type SequenceRestraint<'T> = 'T -> ValidationResult
+
+    type SeqeunceRestraints<'T> = SequenceRestraint<'T> list
+
+    type SequenceRestraintBuilder<'T>() =
+        member __.Yield _ : SeqeunceRestraints<'T> = List.empty
+        member __.Run (validations: SeqeunceRestraints<'T>) (sx:seq<'T>) =
+            let applyValidations (validations: SeqeunceRestraints<'T>) (s:'T) =
+                validations
+                |> List.map (fun v -> v s)
+                |> collectErrors
+            
+            sx
+            |> Seq.map (fun s -> applyValidations validations s)
+            |> Seq.toList
+            |> collectErrors
+            
+        [<CustomOperation "eachElementMust">]
+        member __.EachItemMust<'T> (validations: SeqeunceRestraints<'T>, restraint:('T -> ValidationResult)) =
+            restraint :: validations
+
+    let validSeq<'T> = SequenceRestraintBuilder<'T>()
---
+++ test/Forme.Test/SequenceBuilderTest.fs
@@ -0,0 +1,30 @@
+﻿module Forme.Test.``Sequence Validation Builder``
+
+open NUnit.Framework
+open FsUnit
+open Forme
+
+[<Test>]
+let ``Basic sequence validation should pass`` () =
+    let nonEmptyStrings = validSeq<string> {
+        eachElementMust ValidString.notEmpty
+    }
+
+    ["once"; "like"; "me"]
+    |> nonEmptyStrings
+    |> function
+        | Ok -> Assert.Pass()
+        | ValidationError _ -> failwith "Should have passed"
+
+[<Test>]
+let ``Basic sequence validation should not pass`` () =
+    let nonEmptyStrings = validSeq<string> {
+        eachElementMust ValidString.notEmpty
+    }
+
+    ["once"; ""; "again"]
+    |> nonEmptyStrings
+    |> function
+        | Ok -> Assert.Fail "Should have failed"
+        | ValidationError _ -> Assert.Pass()
+
